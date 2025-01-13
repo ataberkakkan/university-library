f5e434473a46b2d271093e37d023488b1f6416aa
ataberkakkan <akkanataberk@gmail.com>
2025-01-13 12:01:29 +0300

    Book Overview Hero

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,17 @@
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import { sampleBooks } from "@/constants";
+
 export default function Home() {
   return (
     <>
-      <div></div>
+      <BookOverview {...sampleBooks[0]} />
+
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      />
     </>
   );
 }
