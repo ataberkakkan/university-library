59714dcfabf6e3c3227dae7aa38a999723040c46
ataberkakkan <akkanataberk@gmail.com>
2025-01-16 12:25:27 +0300

    Rate Limiting

---
+++ app/too-fast/page.tsx
@@ -0,0 +1,15 @@
+const Page = () => {
+  return (
+    <main className="root-container items-center justify-center">
+      <h1 className="font-bebas-neue text-5xl font-bold text-light-100">
+        Whoa, Slow Down There, Speedy!
+      </h1>
+      <p className="mt-3 max-w-xl text-center text-light-400">
+        Looks like you&apos;ve been a little too eager. We&apos;ve put a
+        temporary pause on your excitement. Chill out for a bit, and try again
+        shortly.
+      </p>
+    </main>
+  );
+};
+export default Page;
